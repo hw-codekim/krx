ad174d14dc0e6235dce31d496318124a9a263acf
hw-codekim <codekim@naver.com>
2024-10-14 00:20:14 +0900

    update dart_conn, db_info

--- key/dart_conn.py
+++ key/dart_conn.py
@@ -1,9 +1,18 @@
 import json
+import os
 
-class dartKey:
-    def dart_key():
-        with open("./key/dart_api_key.json",'r',encoding='utf-8') as f:
-            key = json.load(f)   
-
-        dart_key = key['dart_api_key']
+class DartKey:
+    @staticmethod
+    def get_dart_key():
+        # 환경 변수에서 DART_API_KEY 값을 불러옴
+        dart_key = os.getenv('DART_API_KEY')
+        if dart_key is None:
+            raise ValueError("DART_API_KEY 환경 변수가 설정되지 않았습니다.")
         return dart_key
+# class dartKey:
+#     def dart_key():
+#         with open("./key/dart_api_key.json",'r',encoding='utf-8') as f:
+#             key = json.load(f)   
+
+#         dart_key = key['dart_api_key']
+#         return dart_key
