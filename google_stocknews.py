ba6c42fde7774654616ae52f915a282b852bd3a7
hw-codekim <codekim@naver.com>
2024-10-27 15:12:14 +0900

    google_news adj

--- google_stocknews.py
+++ google_stocknews.py
@@ -7,7 +7,8 @@ import time
 from biz_day import date_biz_day
 from key.db_info import connectDB
 from tqdm import tqdm
-
+import warnings
+warnings.filterwarnings("ignore")
 class google_stocknews:
     def get_krx_daily_price(db_info,biz_day):
         con = pymysql.connect(
@@ -51,19 +52,23 @@ class google_stocknews:
                     pub_date = item.find('pubdate').get_text()
                     pub_date = datetime.strptime(pub_date, '%a, %d %b %Y %H:%M:%S %Z').strftime('%Y-%m-%d')
                     
+                    pub_date = '2024-10-25' #임시
+                    today = '2024-10-25' #임시
+                    
                     if pub_date == today:
                         title = item.find('title').get_text()
                         link = item.find('description')
                         description_soup = BeautifulSoup(link.get_text(), 'html.parser')
                         a_tag = description_soup.find('a')
-                        link = a_tag['href']   
+                        link = a_tag['href']
+ 
                         dd.append([pub_date,title,link])
                 ddf = pd.DataFrame(dd, columns=['기준일','제목','링크'])
+                ddf = ddf[~ddf['제목'].str.contains('조선비즈')]
                 ddf = ddf.sort_values('기준일',ascending=False)
                 ddf.insert(0,'종목명',corp_name)
                 ddf.reset_index(drop=True,inplace=True)
                 ddf = ddf.drop_duplicates('기준일')
-                
                 google_stocknews.insertDB(biz_day,ddf,db_info)
                 news_df = pd.concat([news_df,ddf])
                 # google_stocknews.insertDB(biz_day,news_df,db_info)
